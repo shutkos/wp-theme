d484f15b4e33b5ca73f125013832e4717d2bb182
shutkos <shutkos@gmail.com>
2021-08-16 15:09:11 +0300

    init

--- wp-content/themes/rainbow/functions.php
+++ wp-content/themes/rainbow/functions.php
@@ -1,4 +1,13 @@
 <?php
+
+
+//Подключаем стили и скрипты
 wp_enqueue_style('rainbow', get_template_directory_uri().'/assets/css/rainbow.css');
 wp_enqueue_script('rainbow',get_template_directory_uri().'/assets/js/rainbow.js');
 
+//Регистрируем меню
+add_action('after_setup_theme', 'theme_register_nav_menu');
+
+function theme_register_nav_menu(){
+    register_nav_menu('top', 'Меню в шапке');
+}
